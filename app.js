a5cba419fba42196f1de114fb234b29e08e6f512
andreakao <andreaperez.gz@gmail.com>
2017-10-05 11:33:42 -0300

    Tarjeta

---
+++ app.js
@@ -0,0 +1,13 @@
+function principal(option){
+	do{
+		var respuesta = prompt("Ingrese el número de su tarjeta");
+		if(respuesta = "" || ){
+			alert("Ingrese un número válido")
+		var = num
+
+if(i*2)
+	num = size < 2
+			}
+		var arr = letter
+		if(respuesta = letter)
+	
